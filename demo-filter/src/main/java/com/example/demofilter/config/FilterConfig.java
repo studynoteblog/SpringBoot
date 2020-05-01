7384a10bf3a1a20a7e1bd2b69323b1dcca714280
studynoteblog <qzstudynote@qq.com>
2020-05-01 21:42:20 +0800

    Springboot 过滤器

--- demo-filter/src/main/java/com/example/demofilter/config/FilterConfig.java
+++ demo-filter/src/main/java/com/example/demofilter/config/FilterConfig.java
@@ -1,24 +1,24 @@
-package com.example.demofilter.config;
-
-import com.example.demofilter.filter.MyFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @author 公众号：全栈学习笔记
- * @date 2020/4/30 18:56
- * @description
- */
-@Configuration
-public class FilterConfig {
-    @Bean
-    public FilterRegistrationBean registFilter(){
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(new MyFilter());
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setName("Filter1");
-        registrationBean.setOrder(1);
-        return registrationBean;
-    }
-}
+//package com.example.demofilter.config;
+//
+//import com.example.demofilter.filter.MyFilter;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author 公众号：全栈学习笔记
+// * @date 2020/4/30 18:56
+// * @description
+// */
+//@Configuration
+//public class FilterConfig {
+//    @Bean
+//    public FilterRegistrationBean registFilter(){
+//        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+//        registrationBean.setFilter(new MyFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        registrationBean.setName("Filter1");
+//        registrationBean.setOrder(1);
+//        return registrationBean;
+//    }
+//}
